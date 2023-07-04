2ee1d4660efd9d296d2fa801d4ebbc0ac12797f5
huybang2017 <ndhbang2017@gmail.com>
2023-07-04 23:04:04 +0700

    fixing

--- exercises/basic-of-rust/src/strings.rs
+++ exercises/basic-of-rust/src/strings.rs
@@ -1,16 +1,16 @@
 // Exercise 1
 #[allow(dead_code)]
 fn exercise1(color: &str) -> String {
-    todo!()
+    return color.to_string();
 }
 
 // Exercise 2
 // Fix all errors without adding newline
 fn exercise2() -> String {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
     s.push(',');
-    s.push(" world");
-    s += "!".to_string();
+    s.push_str(" world");
+    s += "!";
     s
 }
 // Exercise 3
@@ -18,7 +18,7 @@ fn exercise2() -> String {
 fn exercise3() -> String {
     let s1 = String::from("hello,");
     let s2 = String::from("world!");
-    let s3 = s1 + s2;
+    let s3 = format!("{} {}", s1, s2);
     s3
 }
 
@@ -26,20 +26,43 @@ fn exercise3() -> String {
 // Reverse a string
 
 fn reverse_string(input: &str) -> String {
-    todo!()
+    if input.len() == 0 {
+        return "".to_string();
+    }
+    let mut count = input.len();
+    let mut result = String::new();
+    while count > 0 {
+        count = count - 1;
+        result.push(input.chars().nth(count).unwrap());
+    }
+    result
 }
-
 
 // Exercise 5
 // Check if a string is a palindrome
 fn is_palindrome(word: &str) -> bool {
-    todo!()
+    let mut flag = false;
+    let mut result = String::new();
+    let work_copy = word.to_string().to_lowercase();
+    for i in word.chars().rev() {
+        result.push(i.to_lowercase().next().unwrap());
+    }
+    if result == work_copy {
+        flag = true
+    }
+    flag
 }
 
 // Exercise 6
 // Count the occurrences of a character in a string
 fn count_char_occurrences(string: &str, ch: char) -> usize {
-    todo!()
+    let mut count = 0;
+    for letter in string.chars().rev() {
+        if letter == ch {
+            count = count + 1;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -63,7 +86,7 @@ mod tests {
     fn exercise3_work() {
         assert_eq!("hello, world!".to_string(), exercise3());
     }
-    
+
     // Test for exercise 4
     #[test]
     fn test_reverse_string() {
@@ -92,8 +115,7 @@ mod tests {
     #[test]
     fn test_count_char_occurrences() {
         assert_eq!(count_char_occurrences("Hello", 'l'), 2);
-        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 2);
         assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
     }
-
 }
